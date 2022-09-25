d306d4b6757aa699a95b9a951801eaea67d46566
LicQuiterio <114039578+LicQuiterio@users.noreply.github.com>
2022-09-25 09:23:53 -0500

    modificacion de mi nombre

--- esta es la buena/src/esta/es/la/buena/EstaEsLaBuena.java
+++ esta es la buena/src/esta/es/la/buena/EstaEsLaBuena.java
@@ -17,7 +17,7 @@ public class EstaEsLaBuena {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "mi nombre es jesus jonas");
+        JOptionPane.showMessageDialog(null, "me llamo Jesus Jonas");
         System.out.println("Soy el LICENCIADO Quiterio ");
     }
     
